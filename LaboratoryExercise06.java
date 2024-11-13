3ad3865591f99e046b8d3a449c740ace7cb1c4fb
Dadaang <danielamuana65@gmail.com>
2024-11-13 09:30:22 +0800

    Add files via upload

---
+++ LaboratoryExercise06.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author PC1
+ */
+public class LaboratoryExercise06 {
+      public static void main(String[] args) {
+          int iNumber = 3;
+          
+          if (iNumber >= 0){
+             
+           System.out.println("first string");
+    
+           System.out.println("second string");
+            }
+    
+         else {
+            }
+           System.out.println("third string");
+      }
+    }
+    
+
